f57ad8a442557c8829f66399b484d826e5d472a3
Bernardo Freguglia <beefreguglia@gmail.com>
2026-03-04 01:52:31 -0300

    feature: create pollyfill

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,13 @@
+/** biome-ignore-all lint/suspicious/noExplicitAny: <using polyfill> */
 import '@testing-library/jest-dom';
-import { TextEncoder } from 'node:util';
 
-global.TextEncoder = TextEncoder;
+import { webcrypto } from 'node:crypto';
+import { TextDecoder, TextEncoder } from 'node:util';
+
+(globalThis as any).TextEncoder = TextEncoder;
+(globalThis as any).TextDecoder = TextDecoder;
+if (!(globalThis as any).crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
 
 expect.extend({});
